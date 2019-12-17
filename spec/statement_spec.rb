294028da3eab29d388a77d8cec7ea847fcadb244
Natalie <49326857+natyeo@users.noreply.github.com>
2019-12-17 16:59:20 +0000

    print statement test mocks time rather than using time.now

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -6,8 +6,8 @@ describe Statement do
 
   describe '#print' do
     it 'prints account statement with headers' do
-      allow(account).to receive(:statement).and_return [{ date: Time.now, transaction: :deposit, amount: 1000, balance: 1000 }, { date: Time.now, transaction: :deposit, amount: 2000, balance: 3000 }, { date: Time.now, transaction: :withdrawal, amount: 500, balance: 2500 }]
-      expect { statement.print_statement }.to output("date || credit || debit || balance\n#{Time.now.strftime('%d/%m/%Y')} || || 500.00 || 2500.00\n#{Time.now.strftime('%d/%m/%Y')} || 2000.00 || || 3000.00\n#{Time.now.strftime('%d/%m/%Y')} || 1000.00 || || 1000.00").to_stdout
+      allow(account).to receive(:statement).and_return [{ date: Time.new(2019, 12, 16), transaction: :deposit, amount: 1000, balance: 1000 }, { date: Time.new(2019, 12, 17), transaction: :deposit, amount: 2000, balance: 3000 }, { date: Time.new(2019, 12, 18), transaction: :withdrawal, amount: 500, balance: 2500 }]
+      expect { statement.print_statement }.to output("date || credit || debit || balance\n18/12/2019 || || 500.00 || 2500.00\n17/12/2019 || 2000.00 || || 3000.00\n16/12/2019 || 1000.00 || || 1000.00").to_stdout
     end
   end
 end
